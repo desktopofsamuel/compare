9235ff355f09006ba8352e9a9f0d128af8672164
Samuel Wong <35249407+desktopofsamuel@users.noreply.github.com>
2021-11-19 12:33:38 +0800

    Add React Table

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,19 @@
+require('dotenv').config({
+  path: `.env.${process.env.NODE_ENV}`,
+});
+
 module.exports = {
   // Since `gatsby-plugin-typescript` is automatically included in Gatsby you
   // don't need to define it here (just if you need to change the options)
+
   plugins: [
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'page',
+        path: `${__dirname}/src/pages/`,
+      },
+    },
     {
       resolve: '@chakra-ui/gatsby-plugin',
       options: {
@@ -15,6 +27,45 @@ module.exports = {
          * if false, this plugin will not use <ColorModeProvider />
          */
         isUsingColorMode: true,
+      },
+    },
+    {
+      resolve: `gatsby-source-airtable`,
+      options: {
+        apiKey: process.env.AIRTABLE_KEY, // may instead specify via env, see below
+        concurrency: 5, // default, see using markdown and attachments for more information
+        tables: [
+          {
+            baseId: `appfnnYQzT5k5TCXE`,
+            tableName: `Value`,
+            // tableView: `YOUR_TABLE_VIEW_NAME`, // optional
+            // queryName: `OPTIONAL_NAME_TO_IDENTIFY_TABLE`, // optionally default is false - makes all records in this table a separate node type, based on your tableView, or if not present, tableName, e.g. a table called "Fruit" would become "allAirtableFruit". Useful when pulling many airtables with similar structures or fields that have different types. See https://github.com/jbolda/gatsby-source-airtable/pull/52.
+            // mapping: { `AppDescription`: `text/markdown` }, // optional, e.g. "text/markdown", "fileNode"
+            tableLinks: [`Apps`, `Features`], // optional, for deep linking to records across tables.
+            // separateNodeType: false, // boolean, default is false, see the documentation on naming conflicts for more information
+            // separateMapType: false, // boolean, default is false, see the documentation on using markdown and attachments for more information
+          },
+          {
+            baseId: `appfnnYQzT5k5TCXE`,
+            tableName: `Apps`,
+            // tableView: `YOUR_TABLE_VIEW_NAME`, // optional
+            // queryName: `OPTIONAL_NAME_TO_IDENTIFY_TABLE`, // optionally default is false - makes all records in this table a separate node type, based on your tableView, or if not present, tableName, e.g. a table called "Fruit" would become "allAirtableFruit". Useful when pulling many airtables with similar structures or fields that have different types. See https://github.com/jbolda/gatsby-source-airtable/pull/52.
+            // mapping: { `AppDescription`: `text/markdown` }, // optional, e.g. "text/markdown", "fileNode"
+            // tableLinks: [`CASE`, `SENSITIVE`, `COLUMN`, `NAMES`], // optional, for deep linking to records across tables.
+            // separateNodeType: false, // boolean, default is false, see the documentation on naming conflicts for more information
+            // separateMapType: false, // boolean, default is false, see the documentation on using markdown and attachments for more information
+          },
+          {
+            baseId: `appfnnYQzT5k5TCXE`,
+            tableName: `Features`,
+            // tableView: `YOUR_TABLE_VIEW_NAME`, // optional
+            // queryName: `OPTIONAL_NAME_TO_IDENTIFY_TABLE`, // optionally default is false - makes all records in this table a separate node type, based on your tableView, or if not present, tableName, e.g. a table called "Fruit" would become "allAirtableFruit". Useful when pulling many airtables with similar structures or fields that have different types. See https://github.com/jbolda/gatsby-source-airtable/pull/52.
+            // mapping: { `AppDescription`: `text/markdown` }, // optional, e.g. "text/markdown", "fileNode"
+            // tableLinks: [`CASE`, `SENSITIVE`, `COLUMN`, `NAMES`], // optional, for deep linking to records across tables.
+            // separateNodeType: false, // boolean, default is false, see the documentation on naming conflicts for more information
+            // separateMapType: false, // boolean, default is false, see the documentation on using markdown and attachments for more information
+          },
+        ],
       },
     },
   ],
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,136 @@
 import React from 'react';
-import { PageProps } from 'gatsby';
-import { Button, Text } from '@chakra-ui/react';
+import { PageProps, graphql } from 'gatsby';
+import {
+  Button,
+  Text,
+  Table,
+  Thead,
+  Tbody,
+  Tfoot,
+  Tr,
+  Th,
+  Td,
+  TableCaption,
+} from '@chakra-ui/react';
 import Link from '@/components/gatsby-link';
 import Title from '@/components/Title';
 import Layout from '@/components/layout';
+import { useTable } from 'react-table';
 
-const Home: React.FC<PageProps> = () => (
-  <Layout title="Hello World">
-    <Title>Hello Gatsby!</Title>
-    <Text>A Chakra UI starter for GatsbyJS.</Text>
-    <Link to="https://twitter.com/desktopofsamuel">
-      <Button>Follow me on Twitter</Button>
-    </Link>
-  </Layout>
-);
+function ReactTable({ columns, data }) {
+  // Use the state and functions returned from useTable to build your UI
+  const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } =
+    useTable({
+      columns,
+      data,
+    });
+
+  // Render the UI for your table
+  return (
+    <table {...getTableProps()}>
+      <thead>
+        {headerGroups.map((headerGroup) => (
+          <tr {...headerGroup.getHeaderGroupProps()}>
+            {headerGroup.headers.map((column) => (
+              <th {...column.getHeaderProps()}>{column.render('Header')}</th>
+            ))}
+          </tr>
+        ))}
+      </thead>
+      <tbody {...getTableBodyProps()}>
+        {rows.map((row, i) => {
+          prepareRow(row);
+          return (
+            <tr {...row.getRowProps()}>
+              {row.cells.map((cell) => {
+                return <td {...cell.getCellProps()}>{cell.render('Cell')}</td>;
+              })}
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+  );
+}
+
+const Home: React.FC<PageProps> = ({ data }) => {
+  const list = data.Table.edges;
+
+  const columns = React.useMemo(
+    () => [
+      {
+        Header: 'Name',
+        columns: [
+          {
+            Header: 'First Name',
+            accessor: 'firstName',
+          },
+          {
+            Header: 'Last Name',
+            accessor: 'lastName',
+          },
+        ],
+      },
+      {
+        Header: 'Info',
+        columns: [
+          {
+            Header: 'Age',
+            accessor: 'age',
+          },
+          {
+            Header: 'Visits',
+            accessor: 'visits',
+          },
+          {
+            Header: 'Status',
+            accessor: 'status',
+          },
+          {
+            Header: 'Profile Progress',
+            accessor: 'progress',
+          },
+        ],
+      },
+    ],
+    [],
+  );
+
+  const data2 = React.useMemo(() => makeData(20), []);
+
+  return (
+    <Layout title="Hello World">
+      <Title>Compare Now</Title>
+      <Text>A Chakra UI starter for GatsbyJS.</Text>
+      <Link to="https://twitter.com/desktopofsamuel">
+        <Button>Follow me on Twitter</Button>
+      </Link>
+      <Table variant="simple">
+        {list.map((item) => (
+          <Thead>{item.node.data.Value}</Thead>
+        ))}
+      </Table>
+      <ReactTable columns={columns} data={data2} />
+    </Layout>
+  );
+};
 
 export default Home;
+
+/* eslint no-undef: "off" */
+export const pageQuery = graphql`
+  query IndexPage {
+    Table: allAirtable {
+      edges {
+        node {
+          id
+          data {
+            ValueKey
+            Value
+          }
+          table
+        }
+      }
+    }
+  }
+`;
